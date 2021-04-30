acf14622ba0eba75f132a5a25d293e131e83cb83
Sergen Tufanoğulları <sergen@whitehorse.technology>
2021-04-30 22:04:25 +0300

    fix

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -3,6 +3,6 @@ var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
 if (accountId && appGuid) {
-    importScripts("https://pdev.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+    importScripts("https://eu-pub.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
 }
         
